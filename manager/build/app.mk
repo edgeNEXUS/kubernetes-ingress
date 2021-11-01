d343c75c271c87c04aa50deca95d71c1fa5c33db
SmartCodeMaker <smartcode@eclipso.eu>
2021-11-01 09:05:30 +0400

    manager: Add makefile

---
+++ manager/build/app.mk
@@ -0,0 +1,25 @@
+
+TARGET_FLAGS += \
+    --add "../lib/Edge/Manager.pm Edge/Manager.pm" \
+    --add "../lib/Edge/ClientAPI/Logging.pm Edge/ClientAPI/Logging.pm" \
+    --add "../lib/Edge/ClientAPI/Object.pm Edge/ClientAPI/Object.pm" \
+    --add "../lib/Edge/ClientAPI/Request.pm Edge/ClientAPI/Request.pm" \
+    --add "../lib/Edge/ClientAPI/async.pm Edge/ClientAPI/async.pm" \
+    --add "../lib/Edge/ClientAPI/Data.pm Edge/ClientAPI/Data.pm" \
+    --add "../lib/Edge/ClientAPI/sync.pm Edge/ClientAPI/sync.pm" \
+    --add "../lib/Edge/ClientAPI/Util/Log.pm Edge/ClientAPI/Util/Log.pm" \
+    --add "../lib/Edge/ClientAPI/Request/FlightPATH.pm Edge/ClientAPI/Request/FlightPATH.pm" \
+    --add "../lib/Edge/ClientAPI/Request/alb_api.pm Edge/ClientAPI/Request/alb_api.pm" \
+    --add "../lib/Edge/ClientAPI/Request/_Base.pm Edge/ClientAPI/Request/_Base.pm" \
+    --add "../lib/Edge/ClientAPI/Request/_Multipart.pm Edge/ClientAPI/Request/_Multipart.pm" \
+    --add "../lib/Edge/ClientAPI/Request/IPServicesApi.pm Edge/ClientAPI/Request/IPServicesApi.pm" \
+    --add "../lib/Edge/ClientAPI/Feed.pm Edge/ClientAPI/Feed.pm" \
+    --add "../lib/Edge/ClientAPI/Creds.pm Edge/ClientAPI/Creds.pm" \
+    --add "../lib/Edge/ClientAPI/E.pm Edge/ClientAPI/E.pm" \
+    --add "../lib/Edge/ClientAPI/Object/VS.pm Edge/ClientAPI/Object/VS.pm" \
+    --add "../lib/Edge/ClientAPI/JSON.pm Edge/ClientAPI/JSON.pm" \
+    --add "../lib/Edge/Manager/Config.pm Edge/Manager/Config.pm" \
+    --add "../lib/Edge/Manager/App.pm Edge/Manager/App.pm" \
+    --add "../lib/Edge/Manager/Watcher.pm Edge/Manager/Watcher.pm" \
+    --add "../lib/Edge/Manager/Static.pm Edge/Manager/Static.pm" \
+    --add "../lib/Edge/ClientAPI.pm Edge/ClientAPI.pm"
