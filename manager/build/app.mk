a5ef3ecbfeabd45317e9836d6c025925c0b9df45
SmartCodeMaker <smartcode@eclipso.eu>
2021-11-01 09:33:09 +0400

    manager: Add all required modules

--- manager/build/app.mk
+++ manager/build/app.mk
@@ -5,6 +5,7 @@ TARGET_FLAGS += \
     --add "../lib/Edge/ClientAPI/Object.pm Edge/ClientAPI/Object.pm" \
     --add "../lib/Edge/ClientAPI/Request.pm Edge/ClientAPI/Request.pm" \
     --add "../lib/Edge/ClientAPI/async.pm Edge/ClientAPI/async.pm" \
+    --add "../lib/Edge/ClientAPI/coro.pm Edge/ClientAPI/coro.pm" \
     --add "../lib/Edge/ClientAPI/Data.pm Edge/ClientAPI/Data.pm" \
     --add "../lib/Edge/ClientAPI/sync.pm Edge/ClientAPI/sync.pm" \
     --add "../lib/Edge/ClientAPI/Util/Log.pm Edge/ClientAPI/Util/Log.pm" \
@@ -14,6 +15,9 @@ TARGET_FLAGS += \
     --add "../lib/Edge/ClientAPI/Request/_Multipart.pm Edge/ClientAPI/Request/_Multipart.pm" \
     --add "../lib/Edge/ClientAPI/Request/IPServicesApi.pm Edge/ClientAPI/Request/IPServicesApi.pm" \
     --add "../lib/Edge/ClientAPI/Feed.pm Edge/ClientAPI/Feed.pm" \
+    --add "../lib/Edge/ClientAPI/Feed/Config.pm Edge/ClientAPI/Feed/Config.pm" \
+    --add "../lib/Edge/ClientAPI/Feed/FPS.pm Edge/ClientAPI/Feed/FPS.pm" \
+    --add "../lib/Edge/ClientAPI/Feed/VSS_FPS.pm Edge/ClientAPI/Feed/VSS_FPS.pm" \
     --add "../lib/Edge/ClientAPI/Creds.pm Edge/ClientAPI/Creds.pm" \
     --add "../lib/Edge/ClientAPI/E.pm Edge/ClientAPI/E.pm" \
     --add "../lib/Edge/ClientAPI/Object/VS.pm Edge/ClientAPI/Object/VS.pm" \
@@ -21,5 +25,12 @@ TARGET_FLAGS += \
     --add "../lib/Edge/Manager/Config.pm Edge/Manager/Config.pm" \
     --add "../lib/Edge/Manager/App.pm Edge/Manager/App.pm" \
     --add "../lib/Edge/Manager/Watcher.pm Edge/Manager/Watcher.pm" \
+    --add "../lib/Edge/Manager/Web.pm Edge/Manager/Web.pm" \
     --add "../lib/Edge/Manager/Static.pm Edge/Manager/Static.pm" \
-    --add "../lib/Edge/ClientAPI.pm Edge/ClientAPI.pm"
+    --add "../lib/Edge/ClientAPI.pm Edge/ClientAPI.pm" \
+    --add "../lib/Edge/Manager/Object/Event.pm Edge/Manager/Object/Event.pm" \
+    --add "../lib/Edge/Manager/HTTPD/Extended.pm Edge/Manager/HTTPD/Extended.pm" \
+    --add "../lib/Edge/Manager/HTTPD/MIME.pm Edge/Manager/HTTPD/MIME.pm" \
+    --add "../lib/Edge/Manager/HTTPD/Extended/Response.pm Edge/Manager/HTTPD/Extended/Response.pm" \
+    --add "../lib/Edge/Manager/HTTPD/Extended/HTTPConnection.pm Edge/Manager/HTTPD/Extended/HTTPConnection.pm" \
+    --add "../lib/Edge/Manager/HTTPD/Extended/HTTPConnection/Writer.pm Edge/Manager/HTTPD/Extended/HTTPConnection/Writer.pm"
